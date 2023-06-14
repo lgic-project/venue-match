461c595bd03c3667efe022f2be3b959983bf6fcb
Samjhana20 <samjhanapoudel255@gmail.com>
2023-06-14 12:44:02 +0545

    modified : destinationtile

--- lib/modules/search_view/widgets/destination_tile.dart
+++ lib/modules/search_view/widgets/destination_tile.dart
@@ -1,58 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/constant.dart';
 import 'package:my_first_app/data/model/destination.dart';
 
 class DestinationTile extends StatelessWidget {
   Destination destination;
 
-  DestinationTile({required this.destination});
+  DestinationTile({super.key, required this.destination});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(15),
+      padding: const EdgeInsets.all(8),
 
       // height: 400,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
+        borderRadius: BorderRadius.circular(5),
         border: Border.all(
-          color: Color.fromARGB(255, 129, 119, 119),
+          color: const Color.fromARGB(255, 129, 119, 119),
         ),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ClipRRect(
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(5),
             child: Image.asset(
               destination.image,
-              width: 200,
-              height: 200,
               fit: BoxFit.cover,
+              height: 150,
+              width: appWidth,
             ),
           ),
-          SizedBox(height: 10),
+          const SizedBox(
+            height: 10,
+          ),
           Text(
             destination.label,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
           ),
-          SizedBox(height: 5),
           Row(
-            children: [
-              const Icon(
+            children: const [
+              Icon(
                 Icons.location_on_outlined,
-                size: 18,
+                size: 12,
               ),
-              const Text(
+              Text(
                 "Pokhara",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                style: TextStyle(
+                  fontSize: 12,
+                ),
               ),
             ],
           ),
-          SizedBox(height: 5),
           Text(
             destination.rupess,
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
           ),
         ],
       ),
