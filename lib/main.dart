e0a75be7ccb2f754c31063b3e39f30d2b709cc1f
Samjhana20 <samjhanapoudel255@gmail.com>
2023-06-17 19:30:06 +0545

    add : venue controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:my_first_app/app_controller.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_first_app/data/controller/app_controller.dart';
 import 'package:my_first_app/constant.dart';
-import 'package:my_first_app/controller/category_controller.dart';
+import 'package:my_first_app/data/controller/category/category_controller.dart';
+import 'package:my_first_app/data/controller/venue/venue_controller.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -26,13 +28,17 @@ class MyApp extends StatelessWidget {
           false, //side ko debug ko banner hatauna lai use gareko
 
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch().copyWith(primary: primary),
-          fontFamily: 'Poppins'),
+        colorScheme: ColorScheme.fromSwatch().copyWith(primary: primary),
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
 
       initialBinding: BindingsBuilder(
         () {
           Get.put(AppController(sharedPreferences), permanent: true);
           Get.put(CategoryController());
+          Get.put(VenueController());
         },
       ),
       home: GetX<AppController>(
