b1a001f7e77fb607ce877b4c6684f47ef8184b30
narvasha <narvasha@gmail.com>
2023-06-18 22:44:12 +0545

    Delete old unwanted files

--- lib/widgets/bottom_navigation.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import '../constant.dart';
-
-class BottomNavigation extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
