b39add9742a265406ceee8a389bb0e035e99c76c
narvasha <narvasha@gmail.com>
2023-06-22 21:39:34 +0545

    constant dart modified

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
-const Color primaryColor = Color(0xff307A59);
-const Color secondary = Color.fromARGB(255, 225, 163, 17);
+const Color primaryColor = Color(0xffFB5C5A);
+const Color secondaryColor = Color(0xFFE1A311);
 const Color lightColor = Color(0xffF5F5F5);
 
 final appHeight = Get.height;
