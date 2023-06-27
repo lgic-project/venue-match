6b31db778bb8570087af36fc31316dcdbdb30af9
Samjhana20 <samjhanapoudel255@gmail.com>
2023-06-27 19:39:21 +0545

    modified : category screen

--- lib/modules/single_category/category_screen.dart
+++ lib/modules/single_category/category_screen.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:my_first_app/data/controller/category/category_controller.dart';
+import 'package:my_first_app/modules/single_venue/single_venue_screen.dart';
 
 import '../../widgets/custom_app_bar_widget.dart';
 import '../../widgets/custom_loading_widget.dart';
 import 'widgets/custom_venue_item_by_category_widget.dart';
 
 class CategoryScreen extends StatelessWidget {
+  final String? id, name;
+  var controller = CategoryController.controller;
+
   CategoryScreen({
     super.key,
     this.id,
     this.name,
   });
-  final String? id, name;
-
-  var controller = CategoryController.controller;
 
   @override
   Widget build(BuildContext context) {
@@ -38,8 +41,16 @@ class CategoryScreen extends StatelessWidget {
                     physics: const BouncingScrollPhysics(),
                     itemBuilder: ((context, index) => Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 5),
-                          child: CustomVenueItemByCategoryWidget(
-                            venues: _.venuesRelatedToCategoryList[index],
+                          child: GestureDetector(
+                            onTap: () => Get.to(() => SingleVenueScreen(
+                                  id: _.venuesRelatedToCategoryList[index].id
+                                      .toString(),
+                                  isFromCategory: true,
+                                  categoryId: id,
+                                )),
+                            child: CustomVenueItemByCategoryWidget(
+                              venues: _.venuesRelatedToCategoryList[index],
+                            ),
                           ),
                         )),
                     itemCount: _.venuesRelatedToCategoryList.length,
