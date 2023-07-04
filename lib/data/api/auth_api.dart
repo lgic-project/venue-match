158b8982e5513f5d7e68b685209e5bff52aedc98
Samjhana20 <samjhanapoudel255@gmail.com>
2023-07-04 18:56:25 +0545

    add : register screen fixed

--- lib/data/api/auth_api.dart
+++ lib/data/api/auth_api.dart
@@ -1,40 +1,12 @@
 import 'dart:convert';
+
 import 'package:get/get.dart';
-import 'package:my_first_app/data/controller/app_controller.dart';
-import 'package:my_first_app/constant.dart';
-import 'package:my_first_app/data/model/login_response.dart';
 import 'package:http/http.dart' as http;
+import 'package:my_first_app/constant.dart';
+import 'package:my_first_app/data/controller/app_controller.dart';
+import 'package:my_first_app/data/model/login_response.dart';
 
 class AuthApi {
-  static Future<LoginResponse> signUp(
-      {required String email,
-      required String password,
-      required String firstName,
-      required String lastName}) async {
-    const url = "${baseUrl}register";
-
-    Map<String, String> requestBody = <String, String>{
-      "email": email,
-      "password": password,
-      "first_name": firstName,
-      "last_name": lastName
-    };
-
-    final response =
-        await http.post(Uri.parse(url), body: requestBody, headers: {
-      "COntent-Type": "application/x-www-form-urlencoded",
-    });
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      LoginResponse loginResponse = LoginResponse.fromJson(data);
-      return loginResponse;
-    } else if (response.statusCode == 401) {
-      throw Exception('some of the fields are missing');
-    } else {
-      throw Exception("something went wrong");
-    }
-  }
-
   static Future<LoginResponse> login(
       {required String email, required String password}) async {
     const url = "${baseUrl}login";
@@ -54,6 +26,36 @@ class AuthApi {
       await Get.find<AppController>()
           .sharedPreferences
           .setString("apiKey", loginResponse.apiKey ?? '');
+      return loginResponse;
+    } else if (response.statusCode == 401) {
+      throw Exception('some of the fields are missing');
+    } else {
+      throw Exception("something went wrong");
+    }
+  }
+
+  static Future<LoginResponse> signUp(
+      {required String email,
+      required String password,
+      required String firstName,
+      required String lastName}) async {
+    const url = "${baseUrl}register";
+
+    Map<String, String> requestBody = <String, String>{
+      "email": email,
+      "password": password,
+      "firstName": firstName,
+      "lastName": lastName,
+      "role": 'user',
+    };
+
+    final response =
+        await http.post(Uri.parse(url), body: requestBody, headers: {
+      "COntent-Type": "application/x-www-form-urlencoded",
+    });
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      LoginResponse loginResponse = LoginResponse.fromJson(data);
       return loginResponse;
     } else if (response.statusCode == 401) {
       throw Exception('some of the fields are missing');
