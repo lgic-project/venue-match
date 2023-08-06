35d4f0a7f9d3e9517a272f95035769f8b01617ab
narvasha <narvasha@gmail.com>
2023-08-06 18:31:02 +0545

    about changes and dish added

--- webapp/src/components/About/Aboutheader.tsx
+++ webapp/src/components/About/Aboutheader.tsx
@@ -17,7 +17,7 @@ export default function Aboutheader() {
                             <div className="position-relative sub-banner-section aos-init aos-animate" data-aos="fade-up" id="myContentDIV">
                                 <h1>About<span className="h1-text"> Us!</span></h1>
                                 <p className="banner-paragraph">
-                                "At Venue Match, we specialize in professional venue management services that are tailored to meet the unique needs of our clients." 
+                                    Lorem ipsum dolor sit amet consectetur adipiscing elit sed doeiusm tempor incididunt 
                                 </p>
                              
                                 <div className="banner-btn discover-btn-banner">
