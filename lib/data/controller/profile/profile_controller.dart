5ab212e25ef9d831e8f3fd8e6b3ab7276aae22c0
Samjhana20 <samjhanapoudel255@gmail.com>
2023-09-13 14:32:06 +0545

    add : profile controller

--- lib/data/controller/profile/profile_controller.dart
+++ lib/data/controller/profile/profile_controller.dart
@@ -1,126 +1,165 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
+import 'dart:convert';
+import 'dart:developer';
 
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:my_first_app/data/model/user_model.dart';
 
+import '../../../constant.dart';
+import '../app_controller.dart';
 
+class ProfileController extends GetxController {
+  static final ProfileController controller = Get.find();
+  bool isLoading = false;
+  String? errorMessage;
 
-// import '../app_controller.dart';
+  var userData = UserModel();
+  ImagePicker imagePicker = ImagePicker();
+  XFile? profilePic;
 
-// class ProfileController extends GetxController {
-//   XFile? updateProfilePic;
+  var firstNameController = TextEditingController();
+  var lastNameController = TextEditingController();
+  var currentPassController = TextEditingController();
+  var newPassController = TextEditingController();
 
-//   var appController = Get.find<AppController>();
+  String? profileImage;
 
-//   // for updating profile
-//   var updateNameController = TextEditingController();
-//   var updatePhnNumController = TextEditingController();
-//   var updateAddressController = TextEditingController();
+  Future changePassword() async {
+    try {
+      var url = '${baseUrl}change-password';
+      Map<String, String> data = {
+        'new_password': newPassController.text,
+        'current_password': currentPassController.text,
+      };
+      log(newPassController.text);
+      log(currentPassController.text);
+      final response = await http.put(
+        Uri.parse(url),
+        body: data,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "api_key": AppController.controller.getToken(),
+        },
+      );
 
-//   // change password
-//   var currentPassController = TextEditingController();
-//   var newPassController = TextEditingController();
+      var authorizationResponse =
+          UserModel.fromJson(json.decode(response.body));
+      if (authorizationResponse.error != null &&
+          authorizationResponse.error == false) {
+        clearFields(isForProfile: false);
+        getUserData();
+        Get.back();
+        Get.rawSnackbar(message: "Password changed successfully");
+      } else {
+        Get.rawSnackbar(message: authorizationResponse.message);
+      }
+    } catch (e) {
+      String errorMessage;
+      if (e.toString().contains("SocketException")) {
+        errorMessage = "No Internet Connection";
+      } else {
+        errorMessage = e.toString();
+      }
+      Get.rawSnackbar(message: errorMessage);
+    }
+  }
 
-//   ImagePicker imagePicker = ImagePicker();
+  clearFields({bool? isForProfile = true}) {
+    if (isForProfile == true) {
+      profilePic = null;
+      profileImage = "";
+      firstNameController.clear();
+      lastNameController.clear();
+    } else {
+      currentPassController.clear();
+      newPassController.clear();
+    }
+  }
 
-//   var userDetailsModel = AuthorizationModel();
+  void getImage() async {
+    profilePic = await imagePicker.pickImage(source: ImageSource.gallery);
+    profileImage = null;
+    update();
+  }
 
-//   void getImage() async {
-//     updateProfilePic = await imagePicker.pickImage(source: ImageSource.gallery);
-//     update();
-//   }
+  Future getUserData() async {
+    try {
+      isLoading = true;
+      var url = "${baseUrl}user-data";
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'api_key': AppController.controller.getToken(),
+        },
+      );
+      userData = UserModel.fromJson(json.decode(response.body));
+      firstNameController.text = userData.firstname ?? '';
+      lastNameController.text = userData.lastname ?? '';
+      profileImage = userData.profilePic;
+      isLoading = false;
+      update();
+    } catch (e) {
+      if (e.toString().contains("SocketException")) {
+        errorMessage = "No Internet Connection";
+      } else {
+        errorMessage = "Failed to load venues";
+      }
+      errorMessage = e.toString();
+    }
+  }
 
-//   Future getProfileDetails() async {
-//     try {
-//       var response = await AuthorizationApi.getUserDetails(
-//         apiKey: appController.apiKey,
-//       );
+  Future udpateProfile() async {
+    try {
+      var url = '${baseUrl}update-profile';
 
-//       if (response.error != null && response.error == false) {
-//         userDetailsModel = response;
-//         update();
-//       } else {
-//         Get.rawSnackbar(message: response.message);
-//       }
-//     } catch (e) {
-//       String errorMessage;
-//       if (e.toString().contains("SocketException")) {
-//         errorMessage = "No Internet Connection";
-//       } else {
-//         errorMessage = e.toString();
-//       }
-//       Get.rawSnackbar(message: errorMessage);
-//     }
-//   }
+      Map<String, String> payload = {
+        'firstname': firstNameController.text,
+        'lastname': lastNameController.text,
+      };
 
-//   clearFields({bool? isForUpdate = true}) {
-//     if (isForUpdate == true) {
-//       updateProfilePic = null;
-//       updateNameController.clear();
-//       updatePhnNumController.clear();
-//       updateAddressController.clear();
-//     } else {
-//       currentPassController.clear();
-//       newPassController.clear();
-//     }
-//   }
+      final request = http.MultipartRequest('POST', Uri.parse(url));
+      request.headers.addAll({
+        "Content-Type": "application/form-data",
+        "api_key": AppController.controller.getToken(),
+      });
 
-//   Future udpateProfile() async {
-//     String updateName = updateNameController.text.trim();
-//     String updatePhnNum = updatePhnNumController.text.trim();
-//     String updateAddress = updateAddressController.text.trim();
+      profilePic == null
+          ? null
+          : request.files.add(
+              await http.MultipartFile.fromPath(
+                "image",
+                profilePic?.path ?? '',
+                contentType: MediaType(
+                  'image',
+                  'jpg',
+                ),
+              ),
+            );
 
-//     try {
-//       var response = await AuthorizationApi.updateProfile(
-//         updateName: updateName,
-//         updatePhnNum: updatePhnNum,
-//         updateAddress: updateAddress,
-//         updateProfilePic: updateProfilePic,
-//       );
+      request.fields.addAll(payload);
+      var data = await request.send();
+      var response = await http.Response.fromStream(data);
 
-//       if (response.error != null && response.error == false) {
-//         clearFields();
-//         getProfileDetails();
-//         Get.back();
-//         Get.rawSnackbar(message: "Profile updated successfully");
-//       } else {
-//         Get.rawSnackbar(message: response.message);
-//       }
-//     } catch (e) {
-//       String errorMessage;
-//       if (e.toString().contains("SocketException")) {
-//         errorMessage = "No Internet Connection";
-//       } else {
-//         errorMessage = e.toString();
-//       }
-//       Get.rawSnackbar(message: errorMessage);
-//     }
-//   }
-
-//   Future changePassword() async {
-//     String newPassword = newPassController.text.trim();
-
-//     try {
-//       var response = await AuthorizationApi.changePassword(
-//         newPassword: newPassword,
-//         apiKey: appController.apiKey,
-//       );
-
-//       if (response.error != null && response.error == false) {
-//         clearFields(isForUpdate: false);
-//         getProfileDetails();
-//         Get.back();
-//         Get.rawSnackbar(message: "Password changed successfully");
-//       } else {
-//         Get.rawSnackbar(message: response.message);
-//       }
-//     } catch (e) {
-//       String errorMessage;
-//       if (e.toString().contains("SocketException")) {
-//         errorMessage = "No Internet Connection";
-//       } else {
-//         errorMessage = e.toString();
-//       }
-//       Get.rawSnackbar(message: errorMessage);
-//     }
-//   }
-// }
+      var profileResponse = UserModel.fromJson(json.decode(response.body));
+      if (profileResponse.error != null && profileResponse.error == false) {
+        clearFields();
+        getUserData();
+        Get.back();
+        Get.rawSnackbar(message: "User Details updated successfully");
+      } else {
+        Get.rawSnackbar(message: profileResponse.message);
+      }
+    } catch (e) {
+      String errorMessage;
+      if (e.toString().contains("SocketException")) {
+        errorMessage = "No Internet Connection";
+      } else {
+        errorMessage = e.toString();
+      }
+      Get.rawSnackbar(message: errorMessage);
+    }
+  }
+}
