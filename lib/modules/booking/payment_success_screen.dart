a695dfcb3c2c74ed3ac91e74fbce6593d2cd879e
Samjhana20 <samjhanapoudel255@gmail.com>
2023-09-16 16:31:17 +0545

    add : set size

--- lib/modules/booking/payment_success_screen.dart
+++ lib/modules/booking/payment_success_screen.dart
@@ -24,6 +24,7 @@ class PaymentSucessScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
